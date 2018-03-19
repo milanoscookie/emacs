bae6ed45907f889e1c7d51c4901a53490b9d9f37
Milan <mmshah2004@gmail.com>
2018-03-19 18:27:13 -0400

    emacs-start-first

---
+++ emacs-dir/.sx/hidden-questions.el
@@ -0,0 +1,1 @@
+(("emacs" 22498) ("stackoverflow"))
---
+++ emacs-dir/modules/private/default/packages.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; -*-
+;;; private/default/packages.el
+
+(package! emacs-snippets
+  :recipe (:fetcher github
+           :repo "hlissner/emacs-snippets"
+           :files ("*")))
+(package! sx)
